320b731cad118f6ed007384ae0fbbcc48504dd1c
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-06-22 19:55:59 +0300

    My job

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,3 +1,13 @@
+/*import { currentAgeForBirthYear } from "../utils";
+
+describe("currentAgeForBirthYear", () => {
+  it("returns the age of a person based on the year of birth", () => {
+    const birthYear = 1984;
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+    expect(ageOfPerson).toBe(38);
+  });
+});*/
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
 // Your code here
+// utils.js
+export const currentAgeForBirthYear = (birthYear) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+};
+
+// test file
+import { currentAgeForBirthYear } from "./utils"; // Make sure to adjust the path if necessary
+
+describe("currentAgeForBirthYear", () => {
+  it("returns the age of a person based on the year of birth", () => {
+    const birthYear = 1984;
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+    expect(ageOfPerson).toBe(38);
+  });
+});
